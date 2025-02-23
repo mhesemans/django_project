fce404e62d05dde01b505808785f3415ed55c820
Micha Hesemans <micha_hesemans@hotmail.com>
2025-02-23 07:18:24 +0000

    feat: added hello world to settings and views in my_project

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Index function
+def index (request):
+    return HttpResponse("Hello, World!")
